4906a405f6b17f9f2e6937d0022ff6fb08ca2fbf
Vivek Thapa <vivekthapa@Viveks-MacBook-Air.local>
2024-03-06 22:11:56 +0000

    first commit

---
+++ src/components/about/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import VideoModal from  '../ModalVideo'
+import abimg2 from '../../images/img-1.png'
+import './style.css'
+
+const About = (props) => {
+    const ClickHandler = () =>{
+        window.scrollTo(10, 0);
+     }
+    return(
+        <div className="about-style-2">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 col-sm-12 col-12">
+                        <div className="about-img">
+                            <img src={props.AbImg} alt=""/>
+                            <VideoModal/>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-sm-12 col-12">
+                        <div className="about-content">
+                            <div className="section-title">
+                                <h2>About Us</h2>
+                            </div>
+                            <p>Skills & Care Greenwich is a registered charity, predominantly working with the older people within the Royal Borough of Greenwich. Since its inception in July 2013, we have successfully delivered a range of projects that have helped over 2000 service users. Skills & care Greenwich is a winner of Civic Award 2016 and Winner of Social Care Award 2016/17 in "Best Charitable Older People Initiative - South East England" category. Our mission is to improve the health and well-being of people in Greenwich by delivering the high-quality services in the community. 
+
+We strive to make a real and lasting difference to the quality of life of local people, helping the community to CONNECT, be  
+
+more ACTIVE, take time to REFLECT, keep on LEARNING, GIVE and CELEBRATE more often and SUPPORT each other.     
+
+Our targeted projects promote community cohesion and increase the health & well-being of local people. </p>
+                            <p> </p>
+                            <div className="btns">
+                                <div><Link  onClick={ClickHandler} className="theme-btn" to="/about">More About Us..</Link></div>
+                            </div>
+                            <div className="signature">
+                                <img src={abimg2} alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/main-component/router/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+
+import Homepage from '../HomePage'
+import Homepage2 from '../HomePage2'
+import Homepage3 from '../HomePage3'
+import AboutPage from '../AboutPage'
+import ShopPage from '../ShopPage'
+import GalleryPage from '../GalleryPage'
+import ShopSinglePage from '../ShopSinglePage'
+import CausePage from '../CasePage'
+import CaseSinglePage from '../CaseSinglePage'
+import EventPage from '../EventPage'
+import EventPageSingle from '../EventPageSingle'
+import DonatePage from '../DonatePage'
+import VolunteerPage from '../volunteerPage'
+import BlogPage from '../BlogPage'
+import BlogPageLeft from '../BlogPageLeft'
+import BlogPageFullwidth from '../BlogPageFullwidth'
+import BlogDetails from '../BlogDetails'
+import BlogDetailsLeftSiide from '../BlogDetailsLeftSiide'
+import BlogDetailsFull from '../BlogDetailsFull'
+import ErrorPage from '../ErrorPage'
+import ContactPage from '../ContactPage'
+import LoginPage from '../LoginPage'
+import SignUpPage from '../SignUpPage'
+import ForgotPassword from '../ForgotPassword'
+
+
+const AllRoute = () => {
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Homepage />} />
+          <Route path='/home' element={<Homepage />} />
+          <Route path='/home2' element={<Homepage2 />} />
+          <Route path='/home3' element={<Homepage3 />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/shop' element={<ShopPage />} />
+          <Route path='/gallery' element={<GalleryPage />} />
+          <Route path='/shop-single' element={<ShopSinglePage />} />
+          <Route path='/case' element={<CausePage />} />
+          <Route path='/case-single' element={<CaseSinglePage />} />
+          <Route path='/event' element={<EventPage />} />
+          <Route path='/event-details' element={<EventPageSingle />} />
+          <Route path='/donate' element={<DonatePage />} />
+          <Route path='/volunteer' element={<VolunteerPage />} />
+          <Route path='/blog' element={<BlogPage />} />
+          <Route path='/blog-left' element={<BlogPageLeft />} />
+          <Route path='/blog-fullwidth' element={<BlogPageFullwidth />} />
+          <Route path='/blog-details' element={<BlogDetails />} />
+          <Route path='/blog-details-left' element={<BlogDetailsLeftSiide />} />
+          <Route path='/blog-details-fullwidth' element={<BlogDetailsFull />} />
+          <Route path='/404' element={<ErrorPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default AllRoute;
